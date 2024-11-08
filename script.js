c886562e8de56a6f3410e51d8d7dd8ff348e6a82
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2024-11-08 17:08:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('form')
+  const list = document.querySelector('#colorSelect')
+
+  form.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const target = e.target.tagName
+
+    if (target === 'INPUT') {
+      const selectedValue = list.selectedIndex
+      if (selectedValue !== -1) {
+        list.remove(selectedValue)
+      }
+    }
+  })
+})
